91c7d7121d9ca57ed4661ee766c5f512eca38810
Mahdi Tavakol <61837861+MahdiTavakol@users.noreply.github.com>
2025-09-23 15:01:57 +0100

    Update compute_diff_atom.cpp
    removed the occasional from the compute_diff_atom.h

--- src/compute_diff_atom.cpp
+++ src/compute_diff_atom.cpp
@@ -64,7 +64,7 @@ void ComputeDiffAtom::init()
   // need neighbors of the ghost atoms
   list_flags |= NeighConst::REQ_GHOST; // I need the ghost atoms to be included in the neighbor list
   // Occasional neighbor list build
-  list_flags |= NeighConst::REQ_OCCASIONAL;
+  // list_flags |= NeighConst::REQ_OCCASIONAL;
 
   // request for a neighbor list
   request = neighbor->add_request(this, list_flags);
